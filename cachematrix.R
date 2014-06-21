a763013d8b820767e8c1004abe30140ebc73ae11
lucylittle <lucylittle14@att.net>
2014-06-21 09:15:44 -0500

    Revert "Programming Assignment 2"
    This reverts commit fc2a9b09fff3a9fbe441c5672e0c57d4111ced0d.

--- cachematrix.R
+++ cachematrix.R
@@ -1,47 +1,15 @@
-makeCacheMatrix <- function(input.matrix = matrix()) {
-        if (!is.matrix(input.matrix)){
-                stop("Please use a matrix with this function")
-        }
-        
-        inverted.matrix <- NULL
-        
-        set <- function(y){
-                
-                input.matrix <<- y
-                
-                inverted.matrix <<- NULL
-        }
-        
-        get <- function() input.matrix
-        
-        set.inverse <- function(solve) inverted.matrix <<- solve
-        
-        get.inverse <- function() inverted.matrix
-        
-        list(set = set,
-             get = get,
-             set.inverse = set.inverse,
-             get.inverse = get.inverse)
-        
+## Put comments here that give an overall description of what your
+## functions do
+
+## Write a short comment describing this function
+
+makeCacheMatrix <- function(x = matrix()) {
+
 }
 
 
-## The following function inverse of the special "matrix" returned
-## by makeCacheMatrix above.
-## If the inverse has already been calculated (and the matrix has not
-## changed), then the cachesolve should retrieve the inverse from the cache.
-## Otherwise it calculate the inverse of the matrix in the cache via
-## the setInvCashe function
+## Write a short comment describing this function
 
-cacheSolve <- function(checkCached.matrix, ...) {
-        
-        inverted.matrix <- checkCached.matrix$get.inverse()
-        if (!is.null(inverted.matrix)){
-                message("Getting Cached Data That Is Already Inverted!")
-                retrun(inverted.matrix)
-        }
-        data <- checkCached.matrix$get()
-        inverted.matrix <- solve(data, ...)
-        checkCached.matrix$set.inverse(inverted.matrix)
-        inverted.matrix
+cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
 }
